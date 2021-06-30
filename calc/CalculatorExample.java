f75515c29fb3c23cb40bdd06cb560668e6fcec34
RyanCD1 <ryandonaghue0@gmail.com>
2021-06-30 11:40:04 +0100

    A first brush with death (or Java by any other name)

---
+++ calc/CalculatorExample.java
@@ -0,0 +1,16 @@
+package calc;
+
+public class CalculatorExample {
+
+	public static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(CalculatorExample.add(1, 2));
+		System.out.println(CalculatorExample.add(11, 2));
+		System.out.println(CalculatorExample.add(18, 2));
+	}
+
+}
